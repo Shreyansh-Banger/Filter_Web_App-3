2bfe2ab8a44ca74f62037f8daefce4cecfc7ae78
Shreyansh-Banger <106863880+Shreyansh-Banger@users.noreply.github.com>
2023-06-11 18:09:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+moustacheX=0;
+moustacheY=0;
+
+function preload() {
+    moustache_nose = loadImage('https://i.postimg.cc/JzGqB24h/Moustache-Image.png');
+}
+
+function setup() {
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNot Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        moustacheX = results[0].pose.nose.x-75;
+        moustacheY = results[0].pose.nose.y;
+        console.log("Moustache x = " + results[0].pose.nose.x);
+        console.log("Moustache y = " + results[0].pose.nose.y);
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(moustache_nose, moustacheX, moustacheY, 150, 50);
+}
+
+function take_snapshot() {
+    save('MyMoustacheFilterImage.jpeg');
+}
